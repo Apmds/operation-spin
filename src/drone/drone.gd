e0ff4989623cdaf49e11192f42b6deaeb0d1eae5
Apmds <ap1mrfj@gmail.com>
2026-06-20 16:04:02 +0100

    up and down movement

--- src/drone/drone.gd
+++ src/drone/drone.gd
@@ -3,7 +3,10 @@ class_name Drone extends CharacterBody3D
 @export var camera: CameraArm
 @onready var mesh: MeshInstance3D = $Mesh
 
-var wind_force = 1.01
+var wind_force = 5
+var vertical_acceleration = 1
+var vertical_decceleration = 1
+var max_vertical_speed = 3
 
 var body_direction: Basis:
 	get: return body_direction
@@ -32,11 +35,11 @@ func _physics_process(delta: float) -> void:
 		var wind_vector = body_direction.y
 		wind_vector = Vector3(wind_vector.x, 0, wind_vector.z)
 		
-		velocity += wind_vector * 5 * delta
+		velocity += wind_vector * wind_force * delta
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	var pitch = input_dir.y * max_tilt
@@ -51,6 +54,12 @@ func _physics_process(delta: float) -> void:
 		#body_direction = Vector3(direction.z, 0, -direction.x) * 0.1
 		#body_direction = Vector3(body_direction.x, camera.get_camera_object().global_rotation.y, body_direction.z)
 		#rotation.y = camera.get_camera_object().global_rotation.y
+	
+	var up_dir := Input.get_axis("move_down", "move_up")
+	if up_dir:
+		velocity.y = move_toward(velocity.y, up_dir*max_vertical_speed, vertical_acceleration*delta)
+	else:
+		velocity.y = move_toward(velocity.y, 0.0, vertical_decceleration*delta)
 	
 	tilt_basis = tilt_basis.rotated(tilt_basis.x, pitch)
 	tilt_basis = tilt_basis.rotated(tilt_basis.z, roll)
