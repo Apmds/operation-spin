a209e2f95742f38d61e8e80aef92bbe77b172ea0
Apmds <ap1mrfj@gmail.com>
2026-06-23 22:37:10 +0100

    very basic menus. win and loose

---
+++ src/documents/documents.gd
@@ -0,0 +1,22 @@
+class_name Documents extends Area3D
+
+@onready var tooltip = $Tooltip
+
+var player_in_range: bool = false
+
+signal documents_grabbed
+
+func _ready() -> void:
+	tooltip.hide()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if player_in_range and event.is_action_pressed("get_documents"):
+		documents_grabbed.emit()
+
+func _on_body_entered(body: Node3D) -> void:
+	tooltip.show()
+	player_in_range = true
+
+func _on_body_exited(body: Node3D) -> void:
+	tooltip.hide()
+	player_in_range = false
