959eda8286d4574c52564d822bdea59aaba894da
goncalo-floro <153073677+goncalofloro@users.noreply.github.com>
2026-06-24 22:01:19 +0100

    added the threshold for sound funcionality

--- src/noise_sensor/noise_sensor.gd
+++ src/noise_sensor/noise_sensor.gd
@@ -7,8 +7,10 @@ extends Node3D
 
 var focused_object: Drone
 var alarm_active: bool = false
-var accumulated_time: float = 0.0
-var threshold_time: float = 3.0
+var game_over_triggered: bool = false
+var noise_threshold: float = 50.0
+var accumulated_noise_time: float = 0.0
+var required_noise_time: float = 3.0
 var sensor_alarm_sound = preload("res://assets/sound_efects/alarm.mp3")
 var whistle_sound: AudioStreamMP3 = preload("res://assets/sound_efects/guard_whistle.mp3")
 
@@ -25,49 +27,69 @@ func _reset_sensor_state() -> void:
 	if focused_object != null and focused_object.boost_changed.is_connected(_on_drone_boost_changed):
 		focused_object.boost_changed.disconnect(_on_drone_boost_changed)
 	focused_object = null
-	accumulated_time = 0.0
-	_stop_alarm_timer()
+	game_over_triggered = false
+	accumulated_noise_time = 0.0
+	alarm_timer.stop()
+	_set_alarm_active(false)
 
-func _start_alarm_timer() -> void:
+func _start_noise_timer() -> void:
 	if focused_object == null:
 		return
 	if alarm_timer.is_stopped():
-		_set_alarm_active(true)
 		alarm_timer.start(0.1)
 
-func _stop_alarm_timer() -> void:
+func _stop_noise_timer() -> void:
 	alarm_timer.stop()
 	_set_alarm_active(false)
 
 func _on_alarm_timeout() -> void:
-	if focused_object != null and focused_object.is_boosted:
-		accumulated_time += 0.1
-		if accumulated_time >= threshold_time:
+	if focused_object == null or game_over_triggered:
+		return
+	if focused_object.noise >= noise_threshold:
+		accumulated_noise_time += 0.1
+		if accumulated_noise_time >= required_noise_time:
+			game_over_triggered = true
+			_set_alarm_active(true)
 			get_tree().change_scene_to_file("res://src/menus/end_menu.tscn")
 			return
+		_set_alarm_active(true)
 		alarm_timer.start(0.1)
+	else:
+		accumulated_noise_time = 0.0
+		_set_alarm_active(false)
 
-func _on_drone_boost_changed(is_boosted: bool) -> void:
+func _on_drone_boost_changed(_is_boosted: bool) -> void:
 	if focused_object == null:
 		return
-	if is_boosted:
-		_start_alarm_timer()
-	else:
-		_stop_alarm_timer()
+	if not focused_object.is_boosted:
+		accumulated_noise_time = 0.0
+		alarm_timer.stop()
+		_set_alarm_active(false)
 
 func _on_detection_area_body_entered(body: Node3D) -> void:
 	if body is Drone:
 		focused_object = body as Drone
+		game_over_triggered = false
+		accumulated_noise_time = 0.0
 		if not focused_object.boost_changed.is_connected(_on_drone_boost_changed):
 			focused_object.boost_changed.connect(_on_drone_boost_changed)
-		if focused_object.is_boosted:
-			_start_alarm_timer()
+		if focused_object.noise >= noise_threshold and focused_object.is_boosted:
+			_start_noise_timer()
 		else:
-			_stop_alarm_timer()
+			_stop_noise_timer()
 
 func _on_detection_area_body_exited(body: Node3D) -> void:
 	if body == focused_object:
 		_reset_sensor_state()
+
+func _process(_delta: float) -> void:
+	if focused_object == null or game_over_triggered:
+		return
+	if focused_object.noise >= noise_threshold and focused_object.is_boosted:
+		_start_noise_timer()
+	else:
+		accumulated_noise_time = 0.0
+		_set_alarm_active(false)
 
 func _ready() -> void:
 	animation_player.play("beep")
