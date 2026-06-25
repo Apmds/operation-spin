dfff97e7d0dba94ef447ddf85349da1adc457f52
Apmds <ap1mrfj@gmail.com>
2026-06-25 11:33:46 +0100

    fix camera detection bug

--- src/security_camera/security_camera.gd
+++ src/security_camera/security_camera.gd
@@ -8,6 +8,16 @@ class_name SecurityCamera extends Node3D
 var focused_object: Node3D
 var focus_rotation_speed = 3
 
+const DETECTION_CONE_ANGLE: float = deg_to_rad(60)
+
+func get_angle_to_focused() -> float:
+	var to_target = (focused_object.global_position - camera_camera_mesh.global_position).normalized()
+	var facing = -camera_camera_mesh.global_basis.z # Godot 3D forward is -Z
+	return facing.angle_to(to_target)
+
+func looking_at_focused() -> bool:
+	return get_angle_to_focused() < DETECTION_CONE_ANGLE/2
+
 # To resume the default behavior (rotating back and forth, be stopped or smth)
 func resume_default() -> void:
 	focused_object = null
@@ -16,7 +26,7 @@ func point_to(obj: Node3D, weight: float) -> void:
 	var a = camera_camera_mesh.basis.slerp(basis_looking_at(position, obj.position), weight)
 	
 	camera_camera_mesh.basis = a
-	detection_area.basis = a
+	#detection_area.basis = a
 
 func basis_looking_at(base_pos: Vector3, target_pos: Vector3) -> Basis:
 	return Basis.looking_at(target_pos - base_pos, Vector3.UP)
@@ -24,11 +34,15 @@ func basis_looking_at(base_pos: Vector3, target_pos: Vector3) -> Basis:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	if focused_object:
-		point_to(focused_object, focus_rotation_speed*delta)
+		if looking_at_focused():
+			point_to(focused_object, focus_rotation_speed*delta)
+			if timer.is_stopped():
+				timer.start()
+	else:
+		timer.stop()
 
 func _on_detection_area_body_entered(body: Node3D) -> void:
 	focused_object = body
-	timer.start()
 
 func _on_detection_area_body_exited(_body: Node3D) -> void:
 	resume_default()
