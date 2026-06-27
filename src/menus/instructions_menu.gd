bdabb5bd02ce8650fb4c1c032f20f0695f81e0d5
Apmds <ap1mrfj@gmail.com>
2026-06-27 12:23:54 +0100

    instructions menu with theme

--- src/menus/instructions_menu.gd
+++ src/menus/instructions_menu.gd
@@ -1,25 +1,14 @@
 extends Control
 
-const AUDIO_SETTINGS_SCENE: PackedScene = preload("res://src/menus/audio_settings_menu.tscn")
-
 @onready var background: TextureRect = $Background
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	background.texture = preload("res://assets/models/city_rooftop_night_skybox_0.png")
-	background.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_COVERED
 	background.pivot_offset = Vector2(background.size.x * 0.5, background.size.y * 0.5)
 	var tween = create_tween()
 	tween.set_loops()
 	tween.tween_property(background, "rotation_degrees", 1.2, 20.0)
 	tween.tween_property(background, "rotation_degrees", -1.2, 20.0)
-	_add_audio_settings_menu()
-
-func _add_audio_settings_menu() -> void:
-	var settings_menu: Control = AUDIO_SETTINGS_SCENE.instantiate()
-	add_child(settings_menu)
-	settings_menu.set_anchors_preset(Control.PRESET_TOP_LEFT)
-	settings_menu.position = Vector2(16, 16)
 
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://src/menus/main_menu.tscn")
